0440987900def9f8e5d5d92ca68ef79e7477f088
Min Sohng <min@cto.ai>
2019-10-28 13:14:32 -0700

    add ci files

---
+++ op-github-e2e.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#set -e -u
+#
+mkdir ~/.ssh/
+touch ~/.ssh/config
+
+cat <<EOT >> ~/.ssh/config
+Host *
+   StrictHostKeyChecking no
+   UserKnownHostsFile=/dev/null
+EOT
+#
+#eval `ssh-agent -s`
+#ssh-add <(echo "${SSH_PRIVATE_KEY}")
+#
+#git config --global url."ssh://git@git.cto.ai:2224/ops/".insteadOf "https://git.cto.ai/ops/"
+
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt update
+
+#echo 'y' |  apt install dialog
+
+echo 'y' |  apt install apt-utils
+
+echo 'y' |  apt install sshpass
+
+echo "DDD"
+
+sshpass -p ${MAC_STADIUM_PASS} ssh -o stricthostkeychecking=no administrator@${MAC_ADDRESS} /Users/administrator/ci_github_script.sh
