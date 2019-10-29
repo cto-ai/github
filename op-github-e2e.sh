c845ba597e2404a5893e4f779b4a409a8ef2d36a
Min Sohng <min@cto.ai>
2019-10-29 12:45:35 -0700

    temp solution for test

--- op-github-e2e.sh
+++ op-github-e2e.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-#set -e -u
-#
-mkdir ~/.ssh/
-touch ~/.ssh/config
+# #set -e -u
+# #
+# mkdir ~/.ssh/
+# touch ~/.ssh/config
 
-cat <<EOT >> ~/.ssh/config
-Host *
-   StrictHostKeyChecking no
-   UserKnownHostsFile=/dev/null
-EOT
-#
-#eval `ssh-agent -s`
-#ssh-add <(echo "${SSH_PRIVATE_KEY}")
-#
-#git config --global url."ssh://git@git.cto.ai:2224/ops/".insteadOf "https://git.cto.ai/ops/"
+# cat <<EOT >> ~/.ssh/config
+# Host *
+#    StrictHostKeyChecking no
+#    UserKnownHostsFile=/dev/null
+# EOT
+# #
+# #eval `ssh-agent -s`
+# #ssh-add <(echo "${SSH_PRIVATE_KEY}")
+# #
+# #git config --global url."ssh://git@git.cto.ai:2224/ops/".insteadOf "https://git.cto.ai/ops/"
 
 
-export DEBIAN_FRONTEND=noninteractive
+# export DEBIAN_FRONTEND=noninteractive
 
-apt update
+# apt update
 
-#echo 'y' |  apt install dialog
+# #echo 'y' |  apt install dialog
 
-echo 'y' |  apt install apt-utils
+# echo 'y' |  apt install apt-utils
 
-echo 'y' |  apt install sshpass
+# echo 'y' |  apt install sshpass
 
-echo "DDD"
+# echo "DDD"
 
-sshpass -p ${MAC_STADIUM_PASS} ssh -o stricthostkeychecking=no administrator@${MAC_ADDRESS} /Users/administrator/ci_github_script.sh
+# sshpass -p ${MAC_STADIUM_PASS} ssh -o stricthostkeychecking=no administrator@${MAC_ADDRESS} /Users/administrator/ci_github_script.sh
+
+./ci_github_script.sh
