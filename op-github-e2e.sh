78379f8383211616a99a5681dc6f1f45219a4b13
Min Sohng <min@cto.ai>
2019-10-30 10:35:25 -0700

    reduce testing

--- op-github-e2e.sh
+++ op-github-e2e.sh
@@ -1,37 +1,30 @@
-#!/bin/bash
+# !/bin/bash
 
-# #set -e -u
-# #
-# mkdir ~/.ssh/
-# touch ~/.ssh/config
+#set -e -u
+#
+mkdir ~/.ssh/
+touch ~/.ssh/config
 
-# cat <<EOT >> ~/.ssh/config
-# Host *
-#    StrictHostKeyChecking no
-#    UserKnownHostsFile=/dev/null
-# EOT
-# #
-# #eval `ssh-agent -s`
-# #ssh-add <(echo "${SSH_PRIVATE_KEY}")
-# #
-# #git config --global url."ssh://git@git.cto.ai:2224/ops/".insteadOf "https://git.cto.ai/ops/"
+cat <<EOT >> ~/.ssh/config
+Host *
+   StrictHostKeyChecking no
+   UserKnownHostsFile=/dev/null
+EOT
+#
+#eval `ssh-agent -s`
+#ssh-add <(echo "${SSH_PRIVATE_KEY}")
+#
+#git config --global url."ssh://git@git.cto.ai:2224/ops/".insteadOf "https://git.cto.ai/ops/"
 
 
-# export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 
-# apt update
+apt update
 
-# #echo 'y' |  apt install dialog
+echo 'y' |  apt install apt-utils
 
-# echo 'y' |  apt install apt-utils
+echo 'y' |  apt install sshpass
 
-# echo 'y' |  apt install sshpass
+echo "DDD"
 
-# echo "DDD"
-
-# sshpass -p ${MAC_STADIUM_PASS} ssh -o stricthostkeychecking=no administrator@${MAC_ADDRESS} /Users/administrator/ci_github_script.sh
-
-npm i -g install @cto.ai/ops
-cd op-github-repo
-npm install 
-npm test
+sshpass -p ${MAC_STADIUM_PASS} ssh -o stricthostkeychecking=no administrator@${MAC_ADDRESS} /Users/administrator/ci_github_script.sh
--- tests_e2e/commands/repo:clone.e2e.ts
+++ tests_e2e/commands/repo:clone.e2e.ts
@@ -1,8 +1,9 @@
-import { run, cleanup } from '../utils/cmd'
+import { run, cleanup, signin, signout } from '../utils/cmd'
 import { ENTER } from '../utils/constants'
 import { execPromisified } from '../../src/helpers/execPromisified'
 
 beforeAll(async () => {
+  await signin()
   await execPromisified('mkdir testrepo')
   process.chdir('testrepo')
 })
@@ -11,6 +12,7 @@ afterAll(async () => {
   await cleanup('repo:clone')
   process.chdir('../')
   await execPromisified('rm -rf testrepo')
+  await signout()
 })
 
 describe('repo:clone happy path', () => {
