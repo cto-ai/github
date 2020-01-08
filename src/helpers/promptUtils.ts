7157d886bb736db4832646f624de407461987819
Vincent Tan <vincent@cto.ai>
2020-01-08 10:49:44 -0800

    Fleshed out keyValPrompt
    Signed-off-by: Vincent Tan <vincent@cto.ai>

--- src/helpers/promptUtils.ts
+++ src/helpers/promptUtils.ts
@@ -6,8 +6,11 @@ import { Question, Questions, ux } from '@cto.ai/sdk';
 export const validatedPrompt = async (prompt: Question | Questions, validate: (response: any) => boolean, errorMessage: string) => {
     let response: any
     let repeatedPrompt = false
+    //keep asking as long as validation fails
     do {
         if (repeatedPrompt) {
+            //not our first prompt
+            //must have errored, so print the error message
             await ux.print(errorMessage)
         }
         response = await ux.prompt(prompt)
@@ -16,12 +19,36 @@ export const validatedPrompt = async (prompt: Question | Questions, validate: (r
     return response
 }
 
+//this replicates the SDK 1.x functionality,
+//where you can have a separate display name and actual value returned
+//prompt should be a ListQuestion (the only one that makes sense, really)
+//name must be unique, as it is used as a key
+//having duplicate name causes undefined behaviour (probably just returns one of the matching at random)
 export const keyValPrompt = async (prompt: Question, choices: { name: string, value: any }[]) => {
-    //TODO: more specific question type
-    //TODO: handle question that only returns 1 val / question that returns 0 or more val
-    const nameList: string[] = choices.map((value) => { return value.name })
-    let filledPrompt = prompt
-    filledPrompt['choices'] = nameList
-    const resp = await ux.prompt(prompt)
+    //the ListQuestion interface is not actually exported
+    if (prompt?.type != 'list') {
+        throw "prompt must be a ListQuestion!"
+    }
 
+    let nameList: string[] = new Array(choices.length)
+    let keyValMap: Map<string, any> = new Map()
+    for (let index = 0; index < choices.length; index++) {
+        //iterate through choices, getting an array of names
+        //and a key-value map
+        const element = choices[index];
+        nameList[index] = element.name;
+        keyValMap.set(element.name, element.value)
+    }
+
+    //have the use select the key...
+    const resp = await ux.prompt(Object.assign(prompt, { choices: nameList }))
+    if (prompt?.name && resp[prompt?.name]) {
+        //and return the associated value
+        // let retVal = {}
+        // retVal[prompt.name] = keyValMap.get(resp[prompt?.name])
+        // return retVal
+        return { [prompt.name]: keyValMap.get(resp[prompt?.name]) };
+    } else {
+        throw "keyValPrompt: Failed to get the value associated with the key"
+    }
 }
