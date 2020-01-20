71251fc63615e35e5c3a2a82c41c76d34d0a9ddb
Vincent Tan <vincent@cto.ai>
2020-01-20 14:53:21 -0800

    Revert debugging-only function from ux.print back to console.log
    Signed-off-by: Vincent Tan <vincent@cto.ai>

--- src/helpers/asyncPipe.ts
+++ src/helpers/asyncPipe.ts
@@ -1,13 +1,10 @@
-import { ux } from '@cto.ai/sdk'
-
 // based on https://github.com/obengwilliam/pipeawait
 
 const asyncPipe = (...fns: Function[]) => (param?: any) =>
   fns.reduce(async (acc, fn) => fn(await acc), param)
 
 const _trace = (msg: string) => (x: any) => {
-  //TODO: this should/should not be async
-  ux.print(msg + ' ' + x)
+  console.log(msg + ' ' + x)
   return x
 }
 
